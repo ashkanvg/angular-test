a50666c6e5155f96bd75412e189f872a9840d95c
Ashkan Vedadi <ashkan.vedadi@live.com>
2020-09-15 15:55:59 +0430

    Single Page Applications Part 1 (2)

--- conFusion/src/app/app-routing/app-routing.module.ts
+++ conFusion/src/app/app-routing/app-routing.module.ts
@@ -17,6 +17,7 @@ import { ContactComponent } from '../contact/contact.component';
 const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'contact',     component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
