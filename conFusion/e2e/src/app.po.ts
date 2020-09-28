d51cb5329cf41cefd6efeb5fb0a6b52f9b1c24a5
Ashkan Vedadi <ashkan.vedadi@live.com>
2020-09-28 15:22:04 +0330

    End-to-End Test

--- conFusion/e2e/src/app.po.ts
+++ conFusion/e2e/src/app.po.ts
@@ -1,11 +1,22 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo(link: string) {
+    return browser.get(link);
+  }
+
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
   }
 
   getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
+  }
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
   }
 }
